807cdca22e642333d500d06e78a5c104328b9bcd
Bruno Correia <bffcorreia@gmail.com>
2018-08-03 18:58:05 +0100

    Add ios frontend.

--- ios/ios/ViewController.swift
+++ ios/ios/ViewController.swift
@@ -1,18 +1,32 @@
 import UIKit
 import Common
+import SDWebImage
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, CommonMemesContractView {
+    
+    private let presenter = CommonMemesPresenter(getMemes: CommonGetMemes(memeRepository: CommonMemeRepository()))
+    
+    @IBOutlet weak var memeView: UIImageView!
+    @IBOutlet weak var titleView: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        let recipe = CommonGetRecipe().run()
-        recipeTitle.text = recipe.title
+        presenter.start(view: self)
     }
-    
-    @IBOutlet weak var recipeTitle: UILabel!
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
+    
+    @IBAction func nextView(_ sender: Any) {
+        presenter.onNextClicked()
+    }
+    
+    func showMemeImage(url: String) {
+        memeView.sd_setImage(with: URL(string: url))
+    }
+    
+    func showMemeTitle(title: String) {
+        titleView.text = title
+    }
 }
-
