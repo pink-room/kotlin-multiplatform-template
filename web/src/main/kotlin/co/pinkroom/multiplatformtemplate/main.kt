4795fd4ba05a5980c849f2c1b18db82c1419e391
Bruno Correia <bffcorreia@gmail.com>
2018-08-03 18:58:23 +0100

    Add web frontend.

--- web/src/main/kotlin/co/pinkroom/multiplatformtemplate/main.kt
+++ web/src/main/kotlin/co/pinkroom/multiplatformtemplate/main.kt
@@ -1,10 +1,5 @@
 package co.pinkroom.multiplatformtemplate
 
-import co.pinkroom.multiplatformtemplate.domain.GetRecipe
-import kotlin.browser.*
-import kotlin.dom.*
-
 fun main(args: Array<String>) {
-    val recipe = GetRecipe().run()
-    document.getElementById("tooltip")?.appendText(recipe.title)
+    Memes()
 }
