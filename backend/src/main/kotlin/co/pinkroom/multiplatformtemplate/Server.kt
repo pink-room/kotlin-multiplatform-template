c3be1afe07fc27d03973fb215cdbd4f2b8bf7db5
Bruno Correia <bffcorreia@gmail.com>
2018-08-06 11:03:53 +0100

    Remove unused code.

--- backend/src/main/kotlin/co/pinkroom/multiplatformtemplate/Server.kt
+++ backend/src/main/kotlin/co/pinkroom/multiplatformtemplate/Server.kt
@@ -1,33 +1,21 @@
 package co.pinkroom.multiplatformtemplate
 
 import co.pinkroom.multiplatformtemplate.controllers.meme
-import co.pinkroom.multiplatformtemplate.controllers.recipe
 import com.google.gson.Gson
 import com.google.gson.GsonBuilder
 import io.ktor.application.Application
-import io.ktor.application.call
 import io.ktor.application.install
 import io.ktor.features.ContentNegotiation
 import io.ktor.gson.GsonConverter
 import io.ktor.http.ContentType
-import io.ktor.response.respondText
 import io.ktor.routing.Routing
-import io.ktor.routing.get
-import io.ktor.routing.routing
 
 fun Application.module() {
     install(ContentNegotiation) {
         register(ContentType.Application.Json, GsonConverter(buildGson()))
-
     }
 
     install(Routing) {
-        routing {
-            get("/") {
-                call.respondText("Hello World!", ContentType.Text.Plain)
-            }
-        }
-        recipe()
         meme()
     }
 }
